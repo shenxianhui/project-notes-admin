d08498eee70aba4f95ad532d150f616a960bf9df
shenxianhui <shenxh@chint.com>
2019-04-29 17:35:20 +0800

    feat: 贪吃蛇(未完成)

--- src/views/game/snake/Snake.vue
+++ src/views/game/snake/Snake.vue
@@ -2,11 +2,11 @@
  * @Author: ShenXianhui
  * @LastEditors: ShenXianhui
  * @Date: 2019-04-25 08:30:57
- * @LastEditTime: 2019-04-26 08:58:16
+ * @LastEditTime: 2019-04-28 11:06:25
  -->
 <!-- 贪吃蛇 -->
 <template>
-    <div class="snake">
+    <div class="snake" ref="snake">
         <div class="map" ref="map">
         </div>
     </div>
@@ -21,9 +21,9 @@ export default {
         return {
             keyNumber: 0, // 键盘码
             timer: null,
-            numDown: 0,
 
             snake: [], // 蛇
+            snakeList: [], // 蛇所占区域坐标(用来判断食物是否生成在蛇内)
             snakeParts: { // 尺寸
                 width: 20,
                 height: 20
@@ -38,10 +38,23 @@ export default {
     watch: {},
     created() {},
     mounted() {
+        this.setMap();
         this.createSnake();
+        this.createFood();
         this.keyboard();
     },
     methods: {
+        // 设置地图大小
+        setMap() {
+            let map = this.$refs.map;
+            let snakeW = this.snakeParts.width;
+            let snakeH = this.snakeParts.height;
+            let borderX = (map.offsetWidth - (Math.floor(map.offsetWidth / snakeW)) * snakeW) / 2 + 'px';
+            let borderY = (map.offsetHeight - (Math.floor(map.offsetHeight / snakeH)) * snakeH) / 2 + 'px';
+
+            this.$refs.snake.style.padding = borderY + ' ' + borderX;
+        },
+
         // 创建🐍
         createSnake() {
             for (let i = 0; i < 3; i++) {
@@ -53,6 +66,7 @@ export default {
                 item.style.width = this.snakeParts.width + 'px';
                 item.style.height = this.snakeParts.height + 'px';
                 item.style.backgroundColor = '#000';
+
                 this.snake.push(item);
                 this.$refs.map.appendChild(item);
             }
@@ -60,6 +74,11 @@ export default {
 
         // 移动
         snakeMove() {
+            // 蛇身跟随
+            for (let i = this.snake.length - 1; i > 0; i--) { // 先让数组末位等于前一位
+                this.snake[i].style.left = this.snake[i - 1].offsetLeft + 'px';
+                this.snake[i].style.top = this.snake[i - 1].offsetTop + 'px';
+            }
             // 蛇头(数组首项)移动
             switch(this.keyNumber) {
                 case 37: // ←
@@ -73,32 +92,31 @@ export default {
                 case 39: // →
                     this.initialPosition.left += this.snakeParts.width;
                     this.snake[0].style.left = this.initialPosition.left + 'px';
-
-                    // 蛇身跟随
-                    for (let i = 1; i < this.snake.length; i++) {
-                        this.snake[i].style.left = this.snake[i - 1].offsetLeft - this.snakeParts.width + 'px';
-                        this.snake[i].style.top = this.snake[i - 1].offsetTop + 'px';
-                    }
                     break;
                 case 40: // ↓
                     this.initialPosition.top += this.snakeParts.height;
                     this.snake[0].style.top = this.initialPosition.top + 'px';
-
-                    // 蛇身跟随
-                    // for (let i = 1; i < this.snake.length; i++) {
-                    //     this.snake[i].style.left = this.snake[i - 1].offsetLeft + 'px';
-                    //     this.snake[i].style.top = this.snake[i - 1].offsetTop - this.snakeParts.height + 'px';
-                    // }
-
-                    // console.log(this.numDown);
-                    // if (this.numDown > this.snake.length) {
-                    //     this.snake[this.numDown + 1].style.left = this.snake[this.numDown].offsetLeft + 'px';
-                    //     console.log(this.snake[0].style.left, this.snake[1].style.left, this.snake[2].style.left);
-                    //     this.numDown++;
-                    // }
-                    // this.snake[this.numDown + 1].style.top = this.snake[this.numDown].offsetTop - this.snakeParts.height + 'px';
                     break;
             }
+        },
+
+        // 创建食物
+        createFood() {
+            let snakeW = this.snakeParts.width;
+            let snakeH = this.snakeParts.height;
+            let randomL = Math.floor((Math.random() * (this.$refs.map.offsetWidth - snakeW)) / snakeW) * snakeW + 'px';
+            let randomT = Math.floor((Math.random() * (this.$refs.map.offsetHeight - snakeH)) / snakeH) * snakeH + 'px';
+            
+            let item = document.createElement("i");
+            item.style.position = 'absolute';
+            item.style.left = randomL;
+            item.style.top = randomT;
+            item.style.display = 'inline-block';
+            item.style.width = this.snakeParts.width + 'px';
+            item.style.height = this.snakeParts.height + 'px';
+            item.style.backgroundColor = '#000';
+
+            this.$refs.map.appendChild(item);
         },
 
         // 键盘事件
@@ -130,6 +148,7 @@ export default {
 
         width: 100%;
         height: 100%;
+        border: 1px solid #000;
     }
 }
 </style>
