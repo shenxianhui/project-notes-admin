a15a850599174123f2fe2dbeea11d39f87fe70eb
shenxianhui <shenxh@chint.com>
2019-06-04 16:54:58 +0800

    feat: 完善地图页

--- src/views/echarts/map/MapSingle.vue
+++ src/views/echarts/map/MapSingle.vue
@@ -6,7 +6,10 @@
  */
 <!-- 中国地图 -->
 <template>
-    <div class="map-single" id="map-single"></div>
+    <div class="container">
+        <span v-show="areaLevel !== 'world'" @click="back()">返回上级</span>
+        <div class="map-single" id="map-single"></div>
+    </div>
 </template>
 
 <script>
@@ -19,108 +22,23 @@ export default {
     props: {},
     data() {
         return {
-            areaName: 'world', // 当前区域-名称
+            areaName: ['World'], // 当前区域-名称
             areaCode: '0', // 当前区域-编号
             areaLevel: 'world', // 当前区域-层级
             map: World, // 加载地图
             option: {
                 backgroundColor: '#154e90',
-                graphic: { // 顶部导航
-                    elements: [
-                        {
-                            type: 'group',
-                            left: 80,
-                            top: 40,
-                            children: [
-                                {
-                                    type: 'line',
-                                    left: 0,
-                                    top: 0,
-                                    shape: {
-                                        x1: 0,
-                                        y1: 0,
-                                        x2: 60,
-                                        y2: 0
-                                    },
-                                    style: {
-                                        stroke: 'rgba(147, 235, 248, .8)'
-                                    }
-                                },
-                                {
-                                    type: 'line',
-                                    left: 0,
-                                    top: 40,
-                                    shape: {
-                                        x1: 0,
-                                        y1: 0,
-                                        x2: 60,
-                                        y2: 0
-                                    },
-                                    style: {
-                                        stroke: 'rgba(147, 235, 248, .8)'
-                                    }
-                                }
-                            ]
-                        },
-                        {
-                            id: 'China',
-                            type: 'group',
-                            left: 90,
-                            top: 50,
-                            children: [
-                                {
-                                    type: 'polyline',
-                                    left: 70,
-                                    top: 5,
-                                    shape: {
-                                        points: [
-                                            [0, 10],
-                                            [8, 20],
-                                            [0, 30]
-                                        ]
-                                    },
-                                    style: {
-                                        // stroke: 'transparent',
-                                        stroke: '#fff'
-                                    }
-                                },
-                                {
-                                    type: 'text',
-                                    left: 0,
-                                    // top: 'middle',
-                                    top: 5,
-                                    style: {
-                                        text: 'World',
-                                        textAlign: 'center',
-                                        fill: '#eee',
-                                        font: '14px "Microsoft YaHei", sans-serif'
-                                    }
-                                },
-                                {
-                                    type: 'text',
-                                    left: 0,
-                                    top: 20,
-                                    style: {
-                                        text: '000000',
-                                        textAlign: 'center',
-                                        fill: '#eee',
-                                        font: '11px "Microsoft YaHei", sans-serif'
-                                    }
-                                }
-                            ]
-                        }
-                    ]
-                },
                 series: [
                     {
                         type: 'map',
-                        map: 'world', // 要与 `registerMap()` 的第一个参数对应
+                        map: 'World', // 要与 `registerMap()` 的第一个参数对应
                         roam: true, // 鼠标缩放+平移
-                        selectedMode: false, // 选中模式
+                        selectedMode: 'single', // 选中
                         label: {
                             show: true,
                             fontSize: 12,
-                            color: '#fff'
+                            color: '#fff',
+                            position: 'inside' // 注: 将地图 JSON 文件的 cp 坐标删掉才有效
                         },
                         itemStyle: {
                             areaColor: {
@@ -129,16 +47,15 @@ export default {
                                 y: 0.5,
                                 r: 0.8,
                                 colorStops: [
-                                    {
+                                    { // 0% 处的颜色
                                         offset: 0,
-                                        color: 'rgba(147, 235, 248, 0)' // 0% 处的颜色
+                                        color: 'rgba(147, 235, 248, 0)'
                                     },
-                                    {
+                                    { // 100% 处的颜色
                                         offset: 1,
-                                        color: 'rgba(147, 235, 248, .2)' // 100% 处的颜色
+                                        color: 'rgba(147, 235, 248, .2)'
                                     }
-                                ],
-                                globalCoord: false // 缺省为 false
+                                ]
                             },
                             borderColor: 'rgba(147, 235, 248, 1)',
                             borderWidth: 1,
@@ -148,6 +65,9 @@ export default {
                             shadowOffsetY: 2
                         },
                         emphasis: { // 高亮状态
+                            label: {
+                                color: '#fff'
+                            },
                             itemStyle: {
                                 areaColor: '#389BB7'
                             }
@@ -159,7 +79,11 @@ export default {
         };
     },
     computed: {},
-    watch: {},
+    watch: {
+        areaLevel() {
+            this.option.series[0].map = this.areaLevel;
+        }
+    },
     mounted() {
         this.initMap();
     },
@@ -180,39 +104,28 @@ export default {
 
             // 点击事件
             myChart.on('click', (e) => {
-                console.log(e);
-                // console.log(AreaCode);
                 this.area = e.name;
                 switch (this.areaLevel) { // 当前地区层级
                     case 'world': // 世界
-                        this.areaName = 'China';
-                        this.areaLevel = 'country';
-                        this.option.series[0].map = 'country';
-
                         this.getCountry(e);
                         break;
                     case 'country': // 国家
-                        this.areaLevel = 'province';
-                        this.option.series[0].map = 'province';
-
                         this.getProvince(e);
                         break;
                     case 'province': // 省
-                        this.areaLevel = 'city';
-                        this.option.series[0].map = 'city';
-
                         this.getCity(e);
                         break;
                     case 'city': // 市
-                        this.areaLevel = 'area';
-                        this.option.series[0].map = 'area';
-
                         this.getArea(e);
+                        // 选中区
+                        if (this.areaLevel === 'area') {
+                            return;
+                        }
                         break;
                     case 'area': // 区
-                        break;
+                        this.getArea(e);
+                        return;
                 }
-                // console.log(this.areaName, this.areaCode, this.areaLevel);
 
                 this.initMap();
             });
@@ -221,18 +134,19 @@ export default {
         // 获取国家
         getCountry(e) {
             this.map = require(`@/data/map/100000`);
-
-            // 修改导航样式
-            this.option.graphic.elements[0].children[0].shape.x2 = 170;
-            this.option.graphic.elements[0].children[1].shape.x2 = 170;
+            this.areaName.push('China');
+            this.areaCode = '1';
+            this.areaLevel = 'country';
         },
 
         // 获取省
         getProvince(e) {
+            this.areaLevel = 'province';
+
             AreaCode.forEach(province => {
                 if (e.name.slice(0, 2) === province.name.slice(0, 2)) {
                     this.areaCode = province.code;
-                    this.areaName = province.name;
+                    this.areaName.push(province.name);
                     this.map = require(`@/data/map/${province.code}0000`);
                 }
             });
@@ -240,11 +154,13 @@ export default {
 
         // 获取市
         getCity(e) {
+            this.areaLevel = 'city';
+
             AreaCode.forEach(province => {
                 province.children.forEach(city => {
                     if (e.name.slice(0, 2) === city.name.slice(0, 2)) {
                         this.areaCode = city.code;
-                        this.areaName = city.name;
+                        this.areaName.push(city.name);
                         this.map = require(`@/data/map/${city.code}00`);
                     }
                 });
@@ -253,15 +169,72 @@ export default {
 
         // 获取区
         getArea(e) {
-            console.log(e);
+            this.areaLevel = 'area';
+
+            AreaCode.forEach(province => {
+                province.children.forEach(city => {
+                    city.children.forEach(area => {
+                        if (e.name.slice(0, 2) === area.name.slice(0, 2)) {
+                            this.areaCode = area.code;
+                            this.areaName[4] = area.name;
+                            // this.map = require(`@/data/map/${area.code}`);
+                        }
+                    });
+                });
+            });
+        },
+
+        // 返回键
+        back() {
+            --this.areaName.length;
+            switch (this.areaLevel) { // 当前地区层级
+                case 'country': // 国家
+                    this.map = require(`@/data/map/000000`);
+                    this.areaCode = '0';
+                    this.areaLevel = 'world';
+                    break;
+                case 'province': // 省
+                    this.map = require(`@/data/map/100000`);
+                    this.areaCode = '1';
+                    this.areaLevel = 'country';
+                    break;
+                case 'city': // 市
+                    this.map = require(`@/data/map/${this.areaCode.slice(0, 2)}0000`);
+                    this.areaCode = `${this.areaCode.slice(0, 2)}`;
+                    this.areaLevel = 'province';
+                    break;
+                case 'area': // 区
+                    this.map = require(`@/data/map/${this.areaCode.slice(0, 4)}00`);
+                    this.areaCode = `${this.areaCode.slice(0, 4)}`;
+                    this.areaLevel = 'city';
+                    break;
+            }
+
+            this.initMap();
         }
     }
 };
 </script>
 
 <style scoped lang='less'>
-.map-single {
+.container {
+    position: relative;
+
     width: 100%;
     height: 100%;
+    span {
+        position: absolute;
+        right: 50px;
+        top: 40px;
+
+        font-size: 20px;
+        color: #fff;
+        cursor: pointer;
+        z-index: 1;
+    }
+    .map-single {
+        width: 100%;
+        height: 100%;
+    }
 }
 </style>
