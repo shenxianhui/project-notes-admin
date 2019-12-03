7a578fe394318e7b35fe1b1fcb56011c0b78ab7a
wells <wells@diancun.net>
2019-12-03 13:40:07 +0800

    conf: 配置文件修改

--- vue.config.js
+++ vue.config.js
@@ -56,7 +56,7 @@ module.exports = {
         ws: true, // 是否启用websockets
         // 开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，
         // 这样服务端和服务端进行数据的交互就不会有跨域问题
-        changOrigin: true,
+        changeOrigin: true,
         pathRewrite: {
           '^/test': ''
         }
