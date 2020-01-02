25ed2de61dcfd347d1d5ec54db84c9e8729197c9
wells <wells@diancun.net>
2020-01-02 18:28:25 +0800

    style: 公共样式修改

--- src/App.vue
+++ src/App.vue
@@ -9,26 +9,6 @@
 </template>
 
 <style lang="less">
-/* 滚动条样式 */
-::-webkit-scrollbar {
-  width: 6px;
-  height: 6px;
-}
-::-webkit-scrollbar-track-piece {
-  background-color: #eee;
-}
-::-webkit-scrollbar-thumb {
-  background: #aaa;
-  min-width: 150px;
-  border-radius: 10px;
-}
-::-webkit-scrollbar-thumb:vertical:hover {
-  background: #555555;
-}
-::-webkit-scrollbar-thumb:horizontal:hover {
-  background: #555555;
-}
-
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
