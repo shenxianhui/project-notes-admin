844198922e8248f8999bc6f9332bd84b12c9f414
wells <wells@diancun.net>
2020-01-09 17:10:58 +0800

    feat: 文件提交

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
-import store from './store';
+import Router from './router';
+import Store from './store';
 import './style/common.less';
 // import '@/config/rem'; // 移动端引入
 
 Vue.config.productionTip = false;
 
 const vm = new Vue({
-  router,
-  store,
+  router: Router,
+  store: Store,
   render: h => h(App)
 }).$mount('#app');
 
-router.beforeEach((to, from, next) => {
+Router.beforeEach((to, from, next) => {
   if (to.matched.length === 0) {
     next({ path: '/error' });
   }
--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,41 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-// 公共
-const Error = () => import(/* webpackChunkName: "error" */ '@/views/common/Error');
-
-const Home = () => import(/* webpackChunkName: "home" */ '@/views/Home');
-const About = () => import(/* webpackChunkName: "about" */ '@/views/About');
-
 Vue.use(VueRouter);
 
-const routes = [
-  // 默认
-  {
-    path: '/',
-    redirect: '/main/home'
-  },
-  // 错误
-  {
-    path: '*',
-    name: 'error',
-    component: Error
-  },
-  {
-    path: '/main',
-    component: { render: f => f('router-view') },
-    children: [
-      {
-        path: 'home',
-        name: 'home',
-        component: Home
-      },
-      {
-        path: 'about',
-        name: 'about',
-        component: About
-      }
-    ]
-  }
-];
-
-const router = new VueRouter({
+export default new VueRouter({
   mode: 'history',
-  routes
+  routes: [
+    // 默认
+    {
+      path: '/',
+      redirect: '/main/home'
+    },
+    // 错误
+    {
+      path: '*',
+      name: 'error',
+      component: loadView('Error')
+    },
+    {
+      path: '/main',
+      component: { render: f => f('router-view') },
+      children: [
+        {
+          path: 'home',
+          name: 'home',
+          component: loadView('Home')
+        },
+        {
+          path: 'about',
+          name: 'about',
+          component: loadView('About')
+        }
+      ]
+    }
+  ]
 });
 
-export default router;
+function loadView(path) {
+  return () => import(/* webpackChunkName: "view-[request]" */ `@/views/${path || ''}`);
+}
