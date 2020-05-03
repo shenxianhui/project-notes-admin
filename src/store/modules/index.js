a6ced472675a189f61a60adc13d569b3c8bbb411
wells <shenxh0928@qq.com>
2020-05-03 15:50:50 +0800

    feat: 文件修改

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,11 +1,11 @@
 const files = require.context('.', true, /\.js$/);
-
-let modules = [];
+let modules = {};
 
 files.keys().forEach(key => {
   if (key === './index.js') return;
 
-  modules = modules.concat(files(key).default); // 读取出文件中的default模块
+  let module = key.split('./')[1].split('.js')[0];
+  modules[module] = require(`./${module}.js`).default;
 });
 
-export default modules; // 抛出一个期待的结构的数组
+export default modules;
