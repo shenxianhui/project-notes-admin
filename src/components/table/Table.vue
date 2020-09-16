0253f38f93b9c64ea61fa7160d40055d431a95d5
wells <shenxh0928@qq.com>
2020-09-16 18:37:38 +0800

    feat: 新增表格组件

---
+++ src/components/table/Table.vue
@@ -0,0 +1,54 @@
+<!--
+ * @Author: shenxh
+ * @Date: 2020-09-16 17:35:09
+ * @LastEditors: shenxh
+ * @LastEditTime: 2020-09-16 18:35:33
+ * @Description: 组件-表格
+-->
+
+<template>
+  <div class="xx-table">
+    <el-table :data="data" border style="width: 100%">
+      <el-table-column
+        v-for="(item, index) in columns"
+        :key="index"
+        :prop="item.prop"
+        :label="item.label"
+        :fixed="item.fixed || false"
+        :width="item.width + ''"
+      >
+        <template slot-scope="scope">
+          {{ scope.row[item.prop] }}
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'xx-table',
+  components: {},
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+    columns: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  beforeDestroy() {},
+  methods: {}
+};
+</script>
+
+<style scoped lang="less"></style>
--- src/views/table/Table.vue
+++ src/views/table/Table.vue
@@ -2,23 +2,73 @@
  * @Author: shenxh
  * @Date: 2020-08-25 09:48:08
  * @LastEditors: shenxh
- * @LastEditTime: 2020-09-12 08:31:23
+ * @LastEditTime: 2020-09-16 18:36:31
  * @Description: 表格
 -->
 
 <template>
   <div class="base-table admin-content">
-    New page1
+    <xx-table :columns="columns" :data="tableData"></xx-table>
   </div>
 </template>
 
 <script>
+import XxTable from '@/components/table';
+
 export default {
   name: 'base-table',
-  components: {},
+  components: {
+    XxTable
+  },
   props: {},
   data() {
-    return {};
+    return {
+      columns: [
+        {
+          label: '姓名',
+          prop: 'name'
+        },
+        {
+          label: '性别',
+          prop: 'sex'
+        },
+        {
+          label: '生日',
+          prop: 'date'
+        },
+        {
+          label: '地址',
+          prop: 'address',
+          width: 500
+        },
+        {
+          label: '操作',
+          fixed: 'right',
+          width: 100,
+          render: () => <a>编辑</a>
+        }
+      ],
+      tableData: [
+        {
+          name: '用户1',
+          sex: 1,
+          date: 816870605000,
+          address: '山东省聊城市冠县'
+        },
+        {
+          name: '用户2',
+          sex: 2,
+          date: 842869125000,
+          address: '浙江省杭州市余杭区'
+        },
+        {
+          name: '用户3',
+          sex: 1,
+          date: 1763288455000,
+          address: '山东省青岛市黄岛区'
+        }
+      ]
+    };
   },
   computed: {},
   watch: {},
