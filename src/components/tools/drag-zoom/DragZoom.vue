698a426d1d4152c99efeed0e30c71d19a0dbf937
shenxh <xianhui.shen@eslink.com>
2022-02-25 17:30:13 +0800

    feat: 新增拖动缩放组件

--- src/components/tools/drag-zoom/DragZoom.vue
+++ src/components/tools/drag-zoom/DragZoom.vue
@@ -2,55 +2,261 @@
  * @Author: shenxh
  * @Date: 2020-07-10 16:40:30
  * @LastEditors: shenxh
- * @LastEditTime: 2022-02-25 11:29:33
+ * @LastEditTime: 2022-02-25 17:29:44
  * @Description: 组件-拖动缩放
 -->
 
 <template>
-  <div ref="xx-drag-zoom" class="xx-drag-zoom">
-    <slot></slot>
-  </div>
+	<div ref="xx-drag-zoom" class="xx-drag-zoom" :style="dragZoomNodeStyle">
+		<slot></slot>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'xx-drag-zoom',
-  components: {},
-  props: {},
-  data() {
-    return {};
-  },
-  computed: {
-    mouseFollow() {
-      return this.$refs['xx-drag-zoom'];
-    }
-  },
-  watch: {},
-  created() {},
-  mounted() {
-    this.mouseFollow.addEventListener('mousedown', evt => {
-      this.mousedown(evt);
-    });
-  },
-  beforeDestroy() {},
-  methods: {
-    mousedown(evt) {
-      let x = evt.clientX - this.mouseFollow.offsetLeft;
-      let y = evt.clientY - this.mouseFollow.offsetTop;
-      document.onmousemove = evt => {
-        this.mouseFollow.style.left = evt.clientX - x + 'px';
-        this.mouseFollow.style.top = evt.clientY - y + 'px';
-      };
-      document.onmouseup = () => {
-        document.onmousemove = null;
-      };
-    }
-  }
+	name: 'xx-drag-zoom',
+	components: {},
+	props: {
+		/* 被操作的元素 start */
+		// X 坐标
+		left: {
+			type: Number,
+			default: 0,
+		},
+		// Y 坐标
+		top: {
+			type: Number,
+			default: 0,
+		},
+		// 宽度
+		width: Number,
+		// 高度
+		height: Number,
+		// 缩放比例
+		zoom: {
+			type: Number,
+			default: 1,
+		},
+		// 最大缩放比例
+		maxZoom: {
+			type: Number,
+			default: 2,
+		},
+		// 最小缩放比例
+		minZoom: {
+			type: Number,
+			default: 0.5,
+		},
+		// 缩放幅度
+		range: {
+			type: Number,
+			default: 0.1,
+		},
+		zoomSpeed: {
+			type: Number,
+			default: 0.5,
+		},
+		/* 被操作的元素 end */
+
+		/* 活动区域 start */
+		// 节点 (注: 传入节点后可以自动获取数据, 不需要再传坐标与宽高了)
+		areaNode: HTMLDivElement,
+		// X 坐标 (未设置 areaNode 时生效)
+		areaLeft: {
+			type: Number,
+			default: 0,
+		},
+		// Y 坐标 (未设置 areaNode 时生效)
+		areaTop: {
+			type: Number,
+			default: 0,
+		},
+		// 宽度 (未设置 areaNode 时生效)
+		areaWidth: {
+			type: Number,
+			default: 200,
+		},
+		// 高度 (未设置 areaNode 时生效)
+		areaHeight: {
+			type: Number,
+			default: 100,
+		},
+		/* 活动区域 end */
+	},
+	data() {
+		return {
+			currentZoom: this.zoom,
+		};
+	},
+	computed: {
+		// 被操作的元素节点
+		dragZoomNode() {
+			return this.$refs['xx-drag-zoom'];
+		},
+		// 活动区域数据
+		areaNodeData() {
+			let obj = {};
+			const node = this.areaNode;
+
+			if (node) {
+				obj = {
+					left: node.clientLeft,
+					top: node.clientTop,
+					width: node.offsetWidth, // 忽略边框
+					height: node.offsetHeight, // 忽略边框
+				};
+			} else {
+				obj = {
+					left: this.areaLeft,
+					top: this.areaTop,
+					width: this.areaWidth,
+					height: this.areaHeight,
+				};
+			}
+
+			return obj;
+		},
+		// 设置样式
+		dragZoomNodeStyle() {
+			return {
+				transform: `scale(${this.currentZoom})`,
+				left: `${-(20 / this.currentZoom)}%`,
+				top: `${-(20 / this.currentZoom)}%`,
+				width: this.width + 'px',
+				height: this.height + 'px',
+			};
+		},
+	},
+	watch: {},
+	created() {},
+	mounted() {
+		this.dragZoomNode.addEventListener('mousedown', this.mousedown);
+		this.dragZoomNode.addEventListener('wheel', this.mousescroll);
+	},
+	beforeDestroy() {
+		this.dragZoomNode.removeEventListener('mousedown');
+		this.dragZoomNode.removeEventListener('wheel');
+	},
+	methods: {
+		// 鼠标点击事件
+		mousedown(evt) {
+			const {
+				// left: areaL,
+				// top: areaT,
+				width: areaW,
+				height: areaH,
+			} = this.areaNodeData;
+			const {
+				offsetLeft: dragL,
+				offsetTop: dragT,
+				offsetWidth: dragW,
+				offsetHeight: dragH,
+			} = this.dragZoomNode;
+			const x = evt.clientX - dragL;
+			const y = evt.clientY - dragT;
+
+			// 鼠标拖动事件
+			document.onmousemove = evt => {
+				const zoom = this.currentZoom;
+				let styleL = evt.clientX - x;
+				let styleT = evt.clientY - y;
+
+				// 当拖动元素宽度小于父元素时
+				if (dragW * zoom < areaW) {
+					// 注: 使用 scale 缩放后, 元素实际尺寸不会改变
+					const boundaryL = (dragW * zoom - dragW) / 2;
+					const boundaryR = areaW - (dragW + boundaryL);
+
+					// 左边界
+					if (styleL < boundaryL) {
+						styleL = boundaryL;
+					}
+					// 右边界
+					if (styleL > boundaryR) {
+						styleL = boundaryR;
+					}
+				} else {
+					// 注: 使用 scale 缩放后, 元素实际尺寸不会改变
+					const boundaryL = (dragW * zoom - dragW) / 2;
+					const boundaryR = -(dragW * zoom - areaW - boundaryL);
+
+					// 左边界
+					if (styleL > boundaryL) {
+						styleL = boundaryL;
+					}
+					// 右边界
+					if (styleL < boundaryR) {
+						styleL = boundaryR;
+					}
+				}
+
+				// 当拖动元素高度小于父元素时
+				if (dragH * zoom < areaH) {
+					// 注: 使用 scale 缩放后, 元素实际尺寸不会改变
+					const boundaryT = (dragH * zoom - dragH) / 2;
+					const boundaryB = areaH - (dragH + boundaryT);
+
+					// 上边界
+					if (styleT < boundaryT) {
+						styleT = boundaryT;
+					}
+					// 下边界
+					if (styleT > boundaryB) {
+						styleT = boundaryB;
+					}
+				} else {
+					// 注: 使用 scale 缩放后, 元素实际尺寸不会改变
+					const boundaryT = (dragH * zoom - dragH) / 2;
+					const boundaryB = -(dragH * zoom - areaH - boundaryT);
+
+					// 上边界
+					if (styleT > boundaryT) {
+						styleT = boundaryT;
+					}
+					// 下边界
+					if (styleT < boundaryB) {
+						styleT = boundaryB;
+					}
+				}
+
+				this.dragZoomNode.style.left = styleL + 'px';
+				this.dragZoomNode.style.top = styleT + 'px';
+			};
+			document.onmouseup = () => {
+				document.onmousemove = null;
+			};
+		},
+
+		// 鼠标滚轮事件
+		mousescroll(evt) {
+			const { deltaY } = evt;
+			let zoom = this.currentZoom;
+
+			// 上滑
+			if (deltaY < 0) {
+				if (zoom >= this.maxZoom) {
+					return;
+				}
+
+				zoom += this.range;
+			} else {
+				if (zoom <= this.minZoom) {
+					return;
+				}
+
+				zoom -= this.range;
+			}
+
+			this.currentZoom = Number(zoom.toFixed(1));
+		},
+	},
 };
 </script>
 
 <style scoped lang="less">
 .xx-drag-zoom {
-  position: absolute;
+	position: absolute;
+	user-select: none;
+	cursor: move;
 }
 </style>
--- src/views/tools/DragZoom.vue
+++ src/views/tools/DragZoom.vue
@@ -2,52 +2,56 @@
  * @Author: shenxh
  * @Date: 2020-07-10 16:40:30
  * @LastEditors: shenxh
- * @LastEditTime: 2022-02-25 11:27:44
+ * @LastEditTime: 2022-02-25 17:29:18
  * @Description: 鼠标拖动
 -->
 
 <template>
-  <div class="drag-zoom">
-    <xx-drag-zoom>
-      <div class="drag-zoom-content">
-        鼠标拖动
-      </div>
-    </xx-drag-zoom>
-  </div>
+	<div class="drag-zoom" ref="drag-zoom">
+		<xx-drag-zoom :area-node="node">
+			<div class="drag-zoom-content">
+				鼠标拖动
+			</div>
+		</xx-drag-zoom>
+	</div>
 </template>
 
 <script>
 import XxDragZoom from '@/components/tools/drag-zoom';
 
 export default {
-  name: 'drag-zoom',
-  components: {
-    XxDragZoom
-  },
-  props: {},
-  data() {
-    return {};
-  },
-  computed: {},
-  watch: {},
-  created() {},
-  mounted() {},
-  beforeDestroy() {},
-  methods: {}
+	name: 'drag-zoom',
+	components: {
+		XxDragZoom,
+	},
+	props: {},
+	data() {
+		return {
+			node: null,
+		};
+	},
+	computed: {},
+	watch: {},
+	created() {},
+	mounted() {
+		this.node = this.$refs['drag-zoom'];
+	},
+	beforeDestroy() {},
+	methods: {},
 };
 </script>
 
 <style scoped lang="less">
 .drag-zoom {
-  width: 100%;
-  height: 100%;
-  .drag-zoom-content {
-    width: 100px;
-    height: 100px;
-    border: 1px solid #000;
-    background: url('~@/assets/logo.png') no-repeat;
-    background-size: 100% 100%;
-    cursor: move;
-  }
+	position: relative;
+	width: 50%;
+	height: 50%;
+	margin: 100px auto;
+	background-color: lightblue;
+	.drag-zoom-content {
+		width: 100px;
+		height: 100px;
+		background: #ff000099;
+	}
 }
 </style>
