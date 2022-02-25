8c9124ed832957cd063abac45ff559ad3b53f676
shenxh <xianhui.shen@eslink.com>
2022-02-25 11:15:08 +0800

    conf: 修改配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,21 @@ module.exports = {
   env: {
     node: true,
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: ['plugin:vue/essential', '@vue/prettier'],
   plugins: ['prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': 1 // 错误提示级别: 0-关闭, 1-警告, 2-报错
+    'prettier/prettier': 1, // 错误提示级别: 0-关闭, 1-警告, 2-报错
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   globals: {
     axios: false,
-    __PROJECTPATH__: false
-  }
+    __PROJECTPATH__: false,
+  },
+  indent: ['error', 'tab'], // 缩进
 };
