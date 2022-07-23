f785c4311d4db2b6d520d00009e519128918e76f
shenxh <shenxh0928@qq.com>
2022-07-23 08:57:24 +0800

    conf: 结尾不使用分号

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'prettier/prettier': 1, // 错误提示级别: 0-关闭, 1-警告, 2-报错
-    'no-unused-vars': 1,
+    'no-unused-vars': 1, // 未使用变量提示
+    semi: [1, 'never'], // 结尾分号
   },
   parserOptions: {
     parser: 'babel-eslint',
@@ -19,4 +20,4 @@ module.exports = {
   globals: {
     // axios: false,
   },
-};
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -2,10 +2,10 @@ module.exports = {
   tabWidth: 2, // 缩进大小
   useTabs: false, // 使用tab缩进
   printWidth: 80, // 每行最大字符数
-  semi: true, // 使用分号
+  semi: false, // 使用分号
   singleQuote: true, // 使用单引号
   arrowParens: 'avoid', // 箭头函数参数括号 avoid|always
   trailingComma: 'all', // 行尾逗号 none|es5|all
   htmlWhitespaceSensitivity: 'css', // html 属性格式化规则 css|strict|ignore
   endOfLine: 'lf', // 行尾序列
-};
+}
