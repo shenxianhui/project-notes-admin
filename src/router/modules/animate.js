0493b0ebeb8cb885a69c177bca358834f37e3a4e
shenxh <shenxh0928@qq.com>
2022-09-19 14:06:08 +0800

    feat: webm

--- src/router/modules/animate.js
+++ src/router/modules/animate.js
@@ -2,40 +2,49 @@
  * @Author: shenxh
  * @Date: 2020-05-01 18:28:06
  * @LastEditors: shenxh
- * @LastEditTime: 2022-02-25 11:20:15
+ * @LastEditTime: 2022-09-19 13:56:50
  * @Description: Animate
  */
+
 export default [
-	{
-		path: '/animate',
-		name: 'animate',
-		meta: {
-			label: '动画',
-		},
-		redirect: '/animate/galaxy',
-		component: loadView('layout/Layout'),
-		children: [
-			{
-				path: 'galaxy',
-				name: 'animate-galaxy',
-				meta: {
-					label: '星系',
-				},
-				component: loadView('animate/galaxy/Galaxy'),
-			},
-			{
-				path: 'serial-frame',
-				name: 'serial-frame',
-				meta: {
-					label: '序列帧',
-				},
-				component: loadView('animate/serial-frame/SerialFrame'),
-			},
-		],
-	},
+  {
+    path: '/animate',
+    name: 'animate',
+    meta: {
+      label: '动画',
+    },
+    redirect: '/animate/galaxy',
+    component: loadView('layout/Layout'),
+    children: [
+      {
+        path: 'galaxy',
+        name: 'animate-galaxy',
+        meta: {
+          label: '星系',
+        },
+        component: loadView('animate/galaxy/Galaxy'),
+      },
+      {
+        path: 'serial-frame',
+        name: 'serial-frame',
+        meta: {
+          label: '序列帧',
+        },
+        component: loadView('animate/serial-frame/SerialFrame'),
+      },
+      {
+        path: 'media-file',
+        name: 'media-file',
+        meta: {
+          label: '媒体文件',
+        },
+        component: loadView('animate/media-file/MediaFile'),
+      },
+    ],
+  },
 ];
 
 function loadView(path) {
-	return () =>
-		import(/* webpackChunkName: "view-[request]" */ `@/views/${path || ''}`);
+  return () =>
+    import(/* webpackChunkName: "view-[request]" */ `@/views/${path || ''}`);
 }
---
+++ src/views/animate/media-file/MediaFile.vue
@@ -0,0 +1,62 @@
+<!--
+ * @Description: 媒体文件
+ * @Author: shenxh
+ * @Date: 2022-09-19 13:54:45
+ * @LastEditors: shenxh
+ * @LastEditTime: 2022-09-19 14:05:31
+-->
+
+<template>
+  <div class="media-file admin-content">
+    <div class="media-file-item">
+      <div class="media-file-item-content">
+        <video
+          autoplay="autoplay"
+          loop="loop"
+          muted="muted"
+          width="184"
+          height="184"
+        >
+          <source src="@/assets/webm/radar.webm" type="video/webm" />
+        </video>
+      </div>
+      <div class="media-file-item-name">.webm</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'media-file',
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  beforeDestroy() {},
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+.media-file {
+  .media-file-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    &:not(:last-child) {
+      padding-bottom: 20px;
+      margin-bottom: 20px;
+      border-bottom: 1px solid #000;
+    }
+    .media-file-item-name {
+      margin-top: 10px;
+      font-size: 18px;
+    }
+  }
+}
+</style>
