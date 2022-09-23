df3a3b3327e87e826f7c960bbfc734a02e842a0e
shenxh <shenxh0928@qq.com>
2022-09-23 17:24:17 +0800

    feat: three组件去除无用代码

--- src/components/three/Three.vue
+++ src/components/three/Three.vue
@@ -3,7 +3,7 @@
  * @Author: shenxh
  * @Date: 2022-08-06 11:09:14
  * @LastEditors: shenxh
- * @LastEditTime: 2022-09-23 16:18:38
+ * @LastEditTime: 2022-09-23 17:21:35
 -->
 
 <template>
@@ -18,7 +18,6 @@
 </template>
 
 <script>
-import * as THREE from 'three';
 import Three from './utils';
 
 let three = null;
@@ -62,42 +61,7 @@ export default {
       if (!three) three = new Three(this.threeId, this.statsId);
 
       /* 模型加载 */
-      // this.initLoader();
       this.$emit('loader-modules', three);
-    },
-
-    // 模型批量加载
-    initLoader() {
-      const context = require.context(
-        '../../../public/modules/water-model',
-        true,
-        /\.glb$/,
-      );
-      const urlList = context.keys().map(item => {
-        return item.replace(/^./, '/modules/water-model');
-      });
-
-      urlList.forEach(item => {
-        const arr = item.split('.');
-        const type = arr[arr.length - 1].toLocaleLowerCase();
-
-        if (type === 'glb' || type === 'gltf') {
-          three.setDRACOLoaderDecoderPath();
-          three.loader.setDRACOLoader(three.dracoLoader);
-
-          three.getGLTFLoaderLoad(
-            item,
-            gltf => {
-              gltf.scene.scale.set(0.01, 0.01, 0.01);
-              three.scene.add(gltf.scene);
-            },
-            undefined,
-            error => {
-              console.error(error);
-            },
-          );
-        }
-      });
     },
 
     // 清空 three
