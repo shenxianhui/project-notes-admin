47d028e86546630d62b9270bc7f442bacc0bb4d1
shenxh <xianhui.shen@eslink.com>
2022-12-01 10:11:50 +0800

    feat: 树克隆

--- src/views/three/water-works-zy.vue
+++ src/views/three/water-works-zy.vue
@@ -3,7 +3,7 @@
  * @Author: shenxh
  * @Date: 2022-09-19 15:10:58
  * @LastEditors: shenxh
- * @LastEditTime: 2022-11-30 13:50:14
+ * @LastEditTime: 2022-11-30 17:14:01
 -->
 
 <template>
@@ -24,7 +24,24 @@ export default {
   },
   props: {},
   data() {
-    return {}
+    return {
+      treeList: [
+        {
+          position: { x: 10900, y: 340, z: 1900 },
+        },
+        {
+          position: { x: 10900, y: 340, z: 2200 },
+        },
+      ],
+      treeList1: [
+        {
+          position: { x: 10700, y: 340, z: 800 },
+        },
+        {
+          position: { x: 10900, y: 340, z: -800 },
+        },
+      ],
+    }
   },
   computed: {},
   watch: {},
@@ -58,122 +75,133 @@ export default {
               const patch = arr[0].split('/')
               const fileName = patch[patch.length - 1]
               let label = ''
-              let position = [0, 0, 0]
+              let position = { x: 0, y: 0, z: 0 }
 
               gltf.name = fileName
               switch (fileName) {
                 // 前池及取水泵房
                 case 'build1YNSCYQ':
                   label = '前池及取水泵房'
-                  position = [-4000, 310, 2500]
+                  position = { x: -4000, y: 310, z: 2500 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 污泥脱水机房
                 case 'build2YNSCYQ':
                   label = '污泥脱水机房'
-                  position = [-7400, 390, -1000]
+                  position = { x: -4000, y: 390, z: -1000 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 储泥池
                 case 'build3YNSCYQ':
                   label = '储泥池'
-                  position = [-9200, 390, -1800]
+                  position = { x: -9200, y: 390, z: -1800 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 污泥浓缩池及排泥泵房
                 case 'build4YNSCYQ':
                   label = '污泥浓缩池及排泥泵房'
-                  position = [-10400, 410, -2500]
+                  position = { x: -10400, y: 410, z: -2500 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 综合楼
                 case 'build5YNSCYQ':
                   label = '综合楼'
-                  position = [10100, 1100, 4450]
+                  position = { x: 10100, y: 1100, z: 4450 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 加药间
                 case 'build6YNSCYQ':
                   label = '加药间'
-                  position = [9670, 250, -3060]
+                  position = { x: 9670, y: 250, z: -3060 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 次氯酸钠投加间
                 case 'build7YNSCYQ':
                   label = '次氯酸钠投加间'
-                  position = [9700, 300, -4300]
+                  position = { x: 9700, y: 300, z: -4300 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 滤池
                 case 'build8YNSCYQ':
                   label = '滤池'
-                  position = [3910, 420, -800]
+                  position = { x: 3910, y: 420, z: -800 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 反应沉淀池
                 case 'build9YNSCYQ':
                   label = '反应沉淀池'
-                  position = [-1970, 220, -830]
+                  position = { x: -1970, y: 220, z: -830 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 高压配电房
                 case 'build10YNSCYQ':
                   label = '高压配电房'
-                  position = [6010, 320, -4700]
+                  position = { x: 6010, y: 320, z: -4700 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 吸水井及送水泵房
                 case 'build11YNSCYQ':
                   label = '吸水井及送水泵房'
-                  position = [3570, 330, -4530]
+                  position = { x: 3570, y: 330, z: -4530 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 门卫室
                 case 'build12YNSCYQ':
                   label = '门卫室'
-                  position = [12698, 230, 1720]
+                  position = { x: 12698, y: 230, z: 1720 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 地面
                 case 'dimianYNSCYQ':
-                  position = [0, 0, 0]
+                  position = { x: 0, y: 0, z: 0 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 地形
                 case 'dixingYNSCYQ':
-                  position = [-3350, 1900, 9000]
+                  position = { x: -3350, y: 1900, z: 9000 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 草地
                 case 'grassYNSCYQ':
-                  position = [2930, 120, 220]
+                  position = { x: 2930, y: 120, z: 220 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 公园
                 case 'qitaYNSCYQ':
-                  position = [10200, 300, 1500]
+                  position = { x: 10200, y: 300, z: 1500 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
                 // 树1
                 case 'tree1COM':
-                  // position = [300, 2000, 0]
-                  // three.scene && three.scene.attach(gltf.scene)
+                  this.treeList.forEach(item => {
+                    const { x, y, z } = item.position
+                    const treeObj = gltf.scene.clone()
+
+                    treeObj.position.set(x, y, z)
+                    three.scene && three.scene.attach(treeObj)
+                  })
                   break
                 // 树2
                 case 'tree2COM':
-                  // position = [0, 2000, 0]
-                  // three.scene && three.scene.attach(gltf.scene)
+                  this.treeList1.forEach(item => {
+                    const { x, y, z } = item.position
+                    const treeObj = gltf.scene.clone()
+
+                    treeObj.position.set(x, y, z)
+                    three.scene && three.scene.attach(treeObj)
+                  })
                   break
                 // 围栏
                 case 'weilanYNSCYQ':
-                  position = [200, 120, 0]
+                  position = { x: 200, y: 120, z: 0 }
                   three.scene && three.scene.attach(gltf.scene)
                   break
               }
               // gltf.scene.scale.set(0.01, 0.01, 0.01)
               // three.scene && three.scene.attach(gltf.scene)
+              const { x, y, z } = position
 
-              gltf.scene.position.set(position[0], position[1], position[2])
+              gltf.scene.position.set(x, y, z)
 
               this.createLabel(label, gltf.scene)
             },
