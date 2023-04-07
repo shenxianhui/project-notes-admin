3f36eb16a55fa2c047719cb50100c407788a0294
shenxh <xianhui.shen@eslink.com>
2023-04-07 15:38:33 +0800

    feat: 元素跳动新增实现方法

--- src/views/css-works/element-dance.vue
+++ src/views/css-works/element-dance.vue
@@ -3,17 +3,19 @@
  * @Author: shenxh
  * @Date: 2023-04-07 14:28:47
  * @LastEditors: shenxh
- * @LastEditTime: 2023-04-07 14:44:25
+ * @LastEditTime: 2023-04-07 15:35:55
 -->
 
 <template>
   <div class="element-dance admin-content">
+    <!-- 方式1 -->
     <div class="element-dance-wrap">
       <div
         v-for="(item, index) in list"
         :key="index"
         class="item"
         :style="{
+          position: 'absolute',
           left: position[index][0] + 'px',
           top: position[index][1] + 'px',
         }"
@@ -22,6 +24,21 @@
           {{ item }}
         </span>
       </div>
+    </div>
+
+    <!-- 方式2 -->
+    <div class="element-dance-wrap">
+      <transition-group
+        class="transition-group"
+        name="transition-group"
+        tag="div"
+      >
+        <div v-for="item in list1" :key="item" class="item">
+          <span>
+            {{ item }}
+          </span>
+        </div>
+      </transition-group>
     </div>
   </div>
 </template>
@@ -36,6 +53,7 @@ export default {
   data() {
     return {
       list: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      list1: [1, 2, 3, 4, 5, 6, 7, 8, 9],
       position: [
         [0, 0],
         [200, 0],
@@ -56,37 +74,33 @@ export default {
   created() {
     timer && clearInterval(timer)
     timer = setInterval(() => {
-      this.position = this.randomSort(this.position)
+      this.position.sort((a, b) => Math.random() - 0.5)
+      this.list1.sort((a, b) => Math.random() - 0.5)
     }, 2000)
   },
   mounted() {},
   beforeDestroy() {
     timer && clearInterval(timer)
   },
-  methods: {
-    // 随机排序
-    randomSort(arr = []) {
-      let tmp = [...arr]
-
-      tmp.sort((a, b) => {
-        return Math.random() - 0.5
-      })
-
-      return tmp
-    },
-  },
+  methods: {},
 }
 </script>
 
 <style lang="less" scoped>
 .element-dance {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
   .element-dance-wrap {
     position: relative;
     display: flex;
     flex-wrap: wrap;
     width: 600px;
+    height: 300px;
+    &:not(:last-child) {
+      margin-bottom: 30px;
+    }
     .item {
-      position: absolute;
       width: 200px;
       height: 100px;
       padding: 10px;
@@ -101,6 +115,13 @@ export default {
         background: #ff000033;
       }
     }
+  }
+  .transition-group {
+    display: flex;
+    flex-wrap: wrap;
+    width: 600px;
+    height: 300px;
+    transition: all 0.4s;
   }
 }
 </style>
